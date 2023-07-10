25a1b236a2b4d82c8f6921744d6803804522c3df
unknown <sputnikuno@outlook.com>
2023-07-10 19:33:12 -0300

    ejercicio 3

--- Ejercicios/src/guia3/CuentaBancaria.java
+++ Ejercicios/src/guia3/CuentaBancaria.java
@@ -18,5 +18,68 @@ j)   Método  consultarDatos() que mostrará todos los datos de la cuenta
 package guia3;
 
 public class CuentaBancaria {
+
+    private int numero, dni;
+    private double saldo;
+
+    CuentaBancaria() {
+    }
+
+    CuentaBancaria(int numero, int dni, double saldo) {
+        this.numero = numero;
+        this.dni = dni;
+        this.saldo = saldo;
+    }
+
+    //getters y setters
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    //metodo ingresar saldo
+    public double ingresarSaldo(double ingreso) {
+        if (ingreso > 0) {
+            saldo += ingreso;
+        } return saldo;
+    }
     
+    //metodo extracción del 20%
+    public double extraccionRapida() {
+        double porcentaje = (saldo * 20) / 100;
+        saldo -= porcentaje;
+
+        return porcentaje;
+    } 
+    
+    //metodo consulta de saldo
+    public void consultarSaldo() {
+        System.out.println("Saldo disponible:" + getSaldo());
+    }
+    
+    //mostrará todos los datos de la cuenta
+    public void consultarDatos() {
+        System.out.println("Número de cuenta" + getNumero());
+        System.out.println("DNI:" + getDni());
+        System.out.println("Saldo disponible:" + getSaldo());        
+    }
 }
